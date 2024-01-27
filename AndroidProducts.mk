6698908571f74c3b91ca5d6d1d7649c24c47692d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-01-27 11:14:56 +0100

    ellis: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm Manufacturer: motorola Model: moto g pure Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_ellis.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_ellis-user \
+    omni_ellis-userdebug \
+    omni_ellis-eng
---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for motorola moto g pure (ellis)
+
+```
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
